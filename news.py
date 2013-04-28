47db74435c75dae26f99d0af0d008767e1ef4ce9
Eric Davis <ed@npri.org>
2013-04-28 12:18:04 -0700

    add group/article support

--- news.py
+++ news.py
@@ -10,6 +10,7 @@ testing requirements:
 import os
 import nntplib
 import collections
+from email.parser import HeaderParser
 
 import utils
 
@@ -28,6 +29,7 @@ class Server(object):
         self.last_response = self.welcome_message
         self.host = host
         self.port = port
+        self.current_group = None
 
     @property
     def welcome_message(self):
@@ -84,6 +86,18 @@ class Server(object):
         else:
             return [line.strip() for line in lines]
 
+    def group(self, name):
+        """Send a GROUP command.
+
+        Selects the active group.
+        """
+        self.last_response = self._server.shortcmd("GROUP %s" % name)
+        (_, count, low, high, name) = self.last_response.split()
+
+        stats = {"low": low, "high": high, "count": count}
+        self.current_group = Group(name, self, stats)
+        return self.current_group
+
     def __repr__(self):
         return "<Server: '%s:%d'>" % (self.host, self.port)
 
@@ -92,7 +106,68 @@ class Server(object):
         return self._server.quit()
 
 class Group(object):
-    pass
+    def __init__(self, name, server, stats):
+        self.name = name
+        self.server = server
+        self._server = server._server
+
+        self.high = stats['high']
+        self.low = stats['low']
+        self.count = stats['count']
+
+    def _get(self, cmd, msg_id):
+        response, results = self._server.longcmd("%s %s" % (cmd, str(msg_id)))
+        self.server.last_response = response
+        if cmd == "HEAD":
+            return Article(headers=results)
+        elif cmd == "BODY":
+            return Article(body=results)
+
+    def head(self, msg_id):
+        return self._get("HEAD", msg_id)
+
+    def body(self, msg_id):
+        return self._get("BODY", msg_id)
+
+    def article(self, msg_id):
+        response, result = self._server.longcmd("ARTICLE %s" % str(msg_id))
+        combined = "\n".join(result)
+        (headers, body) = combined.split("\n\n", 1)
+        return Article(headers, body.lstrip())
+
+    def __repr__(self):
+        return "<Group: '%s' (on %s:%d)>" % (self.name, self.server.host,
+                                             self.server.port)
 
 class Article(object):
-    pass
+    def __init__(self, headers=None, body=None):
+        self.headers = headers
+        self.body = body
+
+        if headers is not None:
+            parser = HeaderParser()
+            headers = self._join_if_needed(headers)
+            self.headers = parser.parsestr(headers, headersonly=True)
+
+        if body is not None:
+            self.body = self._join_if_needed(body)
+
+    def _join_if_needed(self, content):
+        """Normalize how we handle headers/body.
+
+        - HEAD and BODY return lists of strings so we newline join them.
+        - ARTICLE is already joined by newlines, so return as-is.
+        """
+        if isinstance(content, list):
+            return "\n".join(content)
+        elif isinstance(content, basestring):
+            return content
+
+    def save(self, fname):
+        """Save the article's body to fname."""
+        assert self.body is not None, "no body, not writing"
+        with open(os.path.expanduser(fname), "wb") as fp:
+            fp.write(self.body)
+
+    def __repr__(self):
+        return "<Article: '%s'>" % self.headers['message-id']
